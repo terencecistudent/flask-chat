5a7d60495ba3d9fbbceced63fc46b012e6acbffd
terencecistudent <terencelogue100@hotmail.co.uk>
2020-01-13 11:04:52 +0000

    Added chat textbox function ality and refactored code

--- run.py
+++ run.py
@@ -2,24 +2,20 @@ import os
 
 # Are built in python modules
 from datetime import datetime
-from flask import Flask, redirect, render_template, request, session
+from flask import Flask, redirect, render_template, request, session, url_for
 
 app = Flask(__name__)
 app.secret_key = "randomstring123"
 messages = []
 
-def add_messages(username, message):
+def add_message(username, message):
     """ Add messages to messages list """
     # var is now
     # strftime() method takes a date/time object and 
     # then converts that to a string according to a given format.
     now = datetime.now().strftime("%H:%M:%S")
-    messages.append("({}) {}: {}".format(now, username, message))
+    messages.append({"timestamp": now, "from": username, "message": message})
 
-
-def get_all_messages():
-    ''' Get all of the messages and separate them with a br '''
-    return "<br>".join(messages)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -29,22 +25,21 @@ def index():
         session["username"] = request.form["username"]
     
     if "username" in session:
-        return redirect(session["username"])
+        return redirect(url_for("user", username=session["username"]))
 
     return render_template("index.html")
 
 
-@app.route("/<username>")
+@app.route("/chat/<username>", methods=["GET", "POST"])
 def user(username):
-    ''' Display chat messages '''
-    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
+    ''' Add and display chat messages '''
 
+    if request.method == "POST":
+        username = session["username"]
+        message = request.form["message"]
+        add_message(username, message)
+        return redirect(url_for("user", username=session["username"]))
 
-@app.route("/<username>/<message>")
-def send_message(username, message):
-    ''' Create a new message and redirect back to the chat page '''
-    add_messages(username, message)
-    return redirect("/" + username)
-
+    return render_template("chat.html", username=username, chat_messages=messages)
 
 app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
